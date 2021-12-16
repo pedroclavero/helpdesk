5c4fafc36f34fd253a5bec6ebe05b88901d4cf33
pedroclavero <pedro_vitor1234@hotmail.com>
2021-12-16 20:38:16 -0300

    Controladores de exceções e outros problemas corrigidos e metodos modificados da minha maneira

--- src/main/java/com/pedro/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/pedro/helpdesk/controllers/TecnicoController.java
@@ -3,8 +3,8 @@ package com.pedro.helpdesk.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pedro.helpdesk.model.DTOs.TecnicoDTO;
@@ -14,13 +14,20 @@ import com.pedro.helpdesk.services.TecnicoService;
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoController {
-	
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){ 
+
+//	metodo implementado no curso
+//	@GetMapping(value = "/{id}")
+//	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){ 
+//		Tecnico obj = tecnicoService.findById(id);
+//		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+//	}
+
+// metodo implementado e modificado - Pedro Clavero
+	@GetMapping()
+	public ResponseEntity<TecnicoDTO> findById(@RequestParam(name = "id") Integer id) {
 		Tecnico obj = tecnicoService.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
