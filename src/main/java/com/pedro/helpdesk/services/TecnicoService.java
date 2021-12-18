2f7213b098df18f9fb05ea3430aeee5060e12aaf
pedroclavero <pedro_vitor1234@hotmail.com>
2021-12-18 02:08:11 -0300

    Metodo PUT para Tecnico Implementado

--- src/main/java/com/pedro/helpdesk/services/TecnicoService.java
+++ src/main/java/com/pedro/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.pedro.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +23,10 @@ public class TecnicoService {
 	private TecnicoRepository tecnicoRepository;
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElseThrow(
-				() -> new ObjectnotFoundException("Objeto não encontrado!! Id: " + id));
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado!! Id: " + id));
 	}
 
 	public List<Tecnico> findAll() {
@@ -39,15 +40,24 @@ public class TecnicoService {
 		return tecnicoRepository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfeEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return tecnicoRepository.save(oldObj);
+	}
+
 	private void validaPorCpfeEmail(TecnicoDTO objDTO) {
-		Optional<Pessoa> obj =  pessoaRepository.findByCpf(objDTO.getCpf());
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!!");
 		}
-		
+
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!!");
 		}
 	}
+
 }
