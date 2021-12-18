49906dc31ceb8bcca5023b6de73970cf64e123e7
pedroclavero <pedro_vitor1234@hotmail.com>
2021-12-18 02:20:38 -0300

    Delete para tecnico

--- src/main/java/com/pedro/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/pedro/helpdesk/controllers/TecnicoController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,5 +63,11 @@ public class TecnicoController {
 	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO) {
 		Tecnico oldObj = tecnicoService.update(id, objDTO);
 		return ResponseEntity.ok().body(new TecnicoDTO(oldObj));
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id) {
+		tecnicoService.delete(id);
+		return ResponseEntity.noContent().build(); 
 	}
 }
--- src/main/java/com/pedro/helpdesk/services/TecnicoService.java
+++ src/main/java/com/pedro/helpdesk/services/TecnicoService.java
@@ -48,6 +48,14 @@ public class TecnicoService {
 		return tecnicoRepository.save(oldObj);
 	}
 
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de serviço e não pode ser deletado!!");
+		}
+		tecnicoRepository.deleteById(id);
+	}
+
 	private void validaPorCpfeEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
